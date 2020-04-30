89a592f22ade5f1255231162fd3389c786b324ee
Niranjan Dev <Niranjandev4484@gmail.com>
2020-04-30 14:46:30 +0530

    Update UserServiceImpl.java

--- Flight_Management_System/src/main/java/com/cg/service/UserServiceImpl.java
+++ Flight_Management_System/src/main/java/com/cg/service/UserServiceImpl.java
@@ -2,12 +2,10 @@ package com.cg.service;
 
 import java.math.BigInteger;
 import java.util.List;
-
 import org.omg.CORBA.UserException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
-
 import com.cg.dao.UserDaoI;
 import com.cg.entity.User;
 import com.cg.errors.UserCreatedException;
@@ -31,39 +29,33 @@ public class UserServiceImpl implements UserServiceI {
 		}
 	}
 	
-
 	@Override
 	public User viewUser(BigInteger userId) throws UserCreatedException
 	{
 		User user = userdao.viewUser(userId);
-		
 		if(user!=null)
 		{
 			return user;		
 		}
 		else
 			throw new UserCreatedException("Id not found."); 
-
 	}
 		
 	@Override
 	public List<User> viewUsers()
 	{
-		
 		return userdao.viewUsers();
 	}
 		
-
 	@Override
-	public void updateUser(User user) {
+	public void updateUser(User user)
+	{
 		userdao.updateUser(user);
-		
 	}
 
 	@Override
-	public void deleteUser(BigInteger userId) {
-		
-		userdao.deleteUser(userId);
-		
-}
+	public void deleteUser(BigInteger userId) 
+	{	
+		userdao.deleteUser(userId);	
+	}
 }
